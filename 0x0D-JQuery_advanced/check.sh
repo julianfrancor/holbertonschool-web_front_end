539dcfbe5d56f8b1350caf4088f58e92015df6e0
Julian Franco Rua <julianfrancor@gmail.com>
2021-02-23 19:27:57 -0500

    jquery advanced

---
+++ 0x0D-JQuery_advanced/check.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+/home/vagrant/utils/W3C-Validator/w3c_validator.py "0-index.html" "1-index.html" "2-index.html" "3-index.html" "4-index.html" "5-index.html" "6-index.html" "7-index.html" "8-index.html" "9-index.html" "10-index.html, db.json" "11-index.html, db.json" "12-index.html, db.json" 
